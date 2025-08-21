be2e7295240f00d50c0a800b4cd471979ad1a62d
raketen <pyristfeuer@gmail.com>
2025-08-21 16:28:30 +0200

    DANGER: work in progress of matching tokens html doc viewer

--- analyzer/html.py
+++ analyzer/html.py
@@ -1,7 +1,9 @@
+import re
 import pandas as pd
 import dominate
 from dominate.tags import *
 import os
+from dominate.util import text
 
 
 
@@ -30,21 +32,48 @@ def create_html_tr_doc_viewer(filepath: str, suffix: str = ""):
                         'start_idx': row['begin']
                     }
                 with div(id="source-text" if idx == 0 else f"target_text_{idx}"):
-                    h2(row["tafsir_title"])
+                    h2(f"{row['tafsir_title']} ({row['author_death_dce']})")
                     p(f"ID: {row['uid']}")
-                    p(f"Reuses from: {row['src_uid']}")
-                    p(f"Date: {row['author_death_dce']} Author: {row['author_name']}")
-                    p(row["text"])
+                    p(f"Reuses from: {int(row['src_uid']) if not pd.isna(row['src_uid']) else "-"}")
+                    p(f"Author: {row['author_name']}")
+                    target_text_para = p()
                     p("Reuse in source text:")
                     if row['src_uid'] in SC_DICT:
+                        source_text_para = p()
                         src_text = SC_DICT[row["src_uid"]]["text"]
                         # Calculating start position of quote
                         tr_start_idx = int(row["src_begin"] - SC_DICT[row["src_uid"]]["start_idx"])
                         tr_end_idx = int(row["src_end"] - SC_DICT[row["src_uid"]]["start_idx"])
-                        with p():
-                            dominate.util.text(src_text[:tr_start_idx])
-                            span(src_text[tr_start_idx:tr_end_idx], style="background: yellow;")
-                            dominate.util.text(src_text[tr_end_idx:])
+                        # We start creating target and source texts and highlighting matching words
+                        # First, we add the text to the source part that is not included in the text-reuse section
+                        source_text_para += text(src_text[:tr_start_idx])
+                        # Next, we get the slice of source text from which the TR stems
+                        reused_text = src_text[tr_start_idx:tr_end_idx]
+                        reused_text_tokens = reused_text.split(" ")
+                        # DEBUG: Clean tokens
+                        reused_text_tokens = [token for token in reused_text_tokens if len(token) > 1 and token.isalpha()]
+                        print(reused_text_tokens)
+                        # Iterating over token list of target text and highlight similar words in both target and source texts
+                        source_loop_index = 0 # Keep track of position in source list
+                        for token_target in [token for token in row['text'].split(" ") if len(token) > 1 and token.isalpha()]:
+                            print(token_target)
+                            match = False
+                            for inner_idx,token_source in enumerate(reused_text_tokens[source_loop_index:]):
+                                if token_target == token_source:
+                                    # First add the previous tokens
+                                    source_text_para += text(" ".join(reused_text_tokens[source_loop_index:inner_idx]))
+                                    # Mark current word as match
+                                    source_text_para += span(f" {token_source} ", style="background: green;")
+                                    # Set inner loop index
+                                    source_loop_index = inner_idx + 1
+                                    match = True
+                                    break
+                            if match:
+                                target_text_para += span(f" {token_target} ", style="background: green;")
+                            else:
+                                target_text_para += text(f" {token_target} ")
+                        # Add remaining text
+                        source_text_para += text(src_text[tr_end_idx:])
 
     # Save HTML file
     with open(f"analyzer/html/index{suffix}.html", "w") as f:
